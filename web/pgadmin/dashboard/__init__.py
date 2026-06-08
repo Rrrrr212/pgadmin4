ce1f8f65b2eef6ec6054670f1180d3fdaaa6409d
Rrrr <2823785414@qq.com>
2026-06-08 06:16:44 +0000

    7.5.5

--- web/pgadmin/dashboard/__init__.py
+++ web/pgadmin/dashboard/__init__.py
@@ -250,6 +250,9 @@ class DashboardModule(PgAdminModule):
             'dashboard.system_statistics_did',
             'dashboard.replication_slots',
             'dashboard.replication_stats',
+            'dashboard.slow_queries',
+            'dashboard.slow_queries_sid',
+            'dashboard.slow_queries_did',
         ] + pgd_replication.get_exposed_url_endpoints()
 
 
@@ -788,6 +791,33 @@ def replication_slots(sid=None):
         return internal_server_error(errormsg=ERROR_SERVER_ID_NOT_SPECIFIED)
 
     sql = render_template("/".join([g.template_path, 'replication_slots.sql']))
+    status, res = g.conn.execute_dict(sql)
+
+    if not status:
+        return internal_server_error(errormsg=str(res))
+
+    return ajax_response(
+        response=res['rows'],
+        status=200
+    )
+
+
+@blueprint.route('/slow_queries',
+                 endpoint='slow_queries', methods=['GET'])
+@blueprint.route('/slow_queries/<int:sid>',
+                 endpoint='slow_queries_sid', methods=['GET'])
+@blueprint.route('/slow_queries/<int:sid>/<int:did>',
+                 endpoint='slow_queries_did', methods=['GET'])
+@pga_login_required
+@check_precondition
+def slow_queries(sid=None, did=None):
+    if not sid:
+        return internal_server_error(
+            errormsg=ERROR_SERVER_ID_NOT_SPECIFIED)
+
+    sql = render_template(
+        "/".join([g.template_path, 'slow_queries.sql']), did=did
+    )
     status, res = g.conn.execute_dict(sql)
 
     if not status:
