f92ed49d78de9623250708f86f1e65d5fa6c4529
Rrrr <2823785414@qq.com>
2026-06-08 06:19:16 +0000

    7.6.3

--- web/regression/__init__.py
+++ web/regression/__init__.py
@@ -14,8 +14,6 @@ from pgadmin.browser.server_groups.servers.databases.schemas.tests import\
     utils as schema_utils
 from pgadmin.browser.server_groups.servers.databases.schemas.functions.tests\
     import utils as trigger_funcs_utils
-from regression.python_test_utils import test_utils
-from regression.test_setup import config_data
 
 
 global node_info_dict
@@ -37,26 +35,3 @@ parent_node_dict = {
     "schema": [],
     "directory": []
 }
-
-_regression_app = None
-_regression_test_client = None
-
-
-def get_regression_test_client():
-    global _regression_app
-    global _regression_test_client
-
-    if _regression_test_client is None:
-        from regression.runtests import app, test_client
-
-        credentials = config_data.get('pgAdmin4_login_credentials', {})
-        test_client.test_config_data = {
-            'login_username': credentials.get('login_username'),
-            'login_password': credentials.get('login_password')
-        }
-        test_utils.login_using_user_account(test_client)
-
-        _regression_app = app
-        _regression_test_client = test_client
-
-    return _regression_app, _regression_test_client
