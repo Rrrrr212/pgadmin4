6a9b79f5618cb9040b09494a90307c9eb5792a92
Rrrr <2823785414@qq.com>
2026-06-08 06:02:16 +0000

    7.5.4

--- web/pgadmin/dashboard/static/js/Dashboard.jsx
+++ web/pgadmin/dashboard/static/js/Dashboard.jsx
@@ -42,6 +42,7 @@ import CodeMirror from '../../../static/js/components/ReactCodeMirror';
 import GetAppRoundedIcon from '@mui/icons-material/GetAppRounded';
 import DownloadUtils from '../../../static/js/DownloadUtils';
 import RefreshButton from './components/RefreshButtons';
+import SlowQueriesPanel from './SlowQueriesPanel';
 
 function parseData(data) {
   let res = [];
@@ -316,7 +317,7 @@ function Dashboard({
   const prefStore = usePreferences();
   let mainTabs = [gettext('Activity'), gettext('State')];
 
-  mainTabs.push(gettext('Configuration'), gettext('Logs'), gettext('System'));
+  mainTabs.push(gettext('Configuration'), gettext('Logs'), gettext('System'), gettext('Slow Queries'));
   if(treeNodeInfo?.server?.replication_type) {
     mainTabs.push(gettext('Replication'));
   }
@@ -824,7 +825,7 @@ function Dashboard({
   useEffect(() => {
 
     // disable replication tab
-    if(!treeNodeInfo?.server?.replication_type && mainTabVal == 5) {
+    if(!treeNodeInfo?.server?.replication_type && mainTabVal == 6) {
       setMainTabVal(0);
     }
 
@@ -1194,8 +1195,12 @@ function Dashboard({
                   }
                 </Box>
               </TabPanel>
-              {/* Replication */}
+              {/* Slow Queries */}
               <TabPanel value={mainTabVal} index={5} classNameRoot='Dashboard-tabPanel'>
+                <SlowQueriesPanel sid={sid} did={did} />
+              </TabPanel>
+              {/* Replication */}
+              <TabPanel value={mainTabVal} index={6} classNameRoot='Dashboard-tabPanel'>
                 <Replication key={sid} sid={sid} node={node}
                   preferences={preferences} treeNodeInfo={treeNodeInfo} nodeData={nodeData} pageVisible={props.isActive} />
               </TabPanel>
